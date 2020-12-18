3f37558cb317f126eb2d31a292adcb3c7d005e9a
Arunk28 <arunkumar280491@gmail.com>
2020-12-18 22:03:42 +0530

    web pack configuration

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <>
+    <h1>React</h1>
+    <p>web pack configuration</p>
+  </>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
